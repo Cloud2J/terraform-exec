58251ae9d72583cbd380bafb8d4f02056c80ef93
Jefferson Madrid <jefferson.imadrid@gmail.com>
2020-05-10 17:32:30 +1000

    use variables for aws educate account

--- variables.tf
+++ variables.tf
@@ -8,17 +8,18 @@ locals {
 }
 
 variable "vpc-main" {
-  default = "vpc-fccdcd86"
+  default = "vpc-576b7c2d" # << aws educate account vpc
+  #"vpc-fccdcd86" << my personal vpc
 }
 
 variable "subnet-id-map" {
   default = {
-    "us-east-1a" = "subnet-7014e016"
-    "us-east-1b" = "subnet-91fa08b0"
-    "us-east-1c" = "subnet-3ee56273"
-    "us-east-1d" = "subnet-64a3533b"
-    "us-east-1e" = "subnet-44447f7a"
-    "us-east-1f" = "subnet-5704aa59"
+    "us-east-1a" = "subnet-e436f2bb" #"subnet-7014e016" << personal account subnet
+    "us-east-1b" = "subnet-35887353" #"subnet-91fa08b0"
+    "us-east-1c" = "subnet-246fa905" #"subnet-3ee56273"
+    "us-east-1d" = "subnet-f775feba" #"subnet-64a3533b"
+    "us-east-1e" = "subnet-91bfb0af" #"subnet-44447f7a"
+    "us-east-1f" = "subnet-366dd038" #"subnet-5704aa59"
   }
 }
 
